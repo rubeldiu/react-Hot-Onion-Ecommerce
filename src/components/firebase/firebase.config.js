f35f7d27b545f447fbf1e9abc982f14fd3713236
hamiduldiu <hamiduldiu@gmail.com>
2021-04-10 15:13:03 +0200

    ecommerce site demo

---
+++ src/components/firebase/firebase.config.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAD2_1m3pAgmZzjYCWDoHraJmHx-PuGK04",
+    authDomain: "red-onion-hamidul.firebaseapp.com",
+    projectId: "red-onion-hamidul",
+    storageBucket: "red-onion-hamidul.appspot.com",
+    messagingSenderId: "26532935734",
+    appId: "1:26532935734:web:835cac005dc8449d974097"
+};
+
+export default firebaseConfig;
